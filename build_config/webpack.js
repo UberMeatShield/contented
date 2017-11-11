bac36e8d1920433fcdf4273522abbfe7f7c666a8
Justin Carlson <jcarlson@secureworks.com>
2017-11-11 18:01:07 -0500

    Ports to angular 5, needs to add in some tests in the near future.

--- build_config/webpack.js
+++ build_config/webpack.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     plugins: [
          new webpack.ContextReplacementPlugin(
-             /angular(\\|\/)core(\\|\/)@angular/,
+             /(.+)?angular(\\|\/)core(.+)?/,
              path.resolve(__dirname, '../src')
          )
     ],
--- src/ts/contented/contented_service.ts
+++ src/ts/contented/contented_service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, URLSearchParams, Request, RequestOptions, Headers} from '@angular/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpErrorResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 import * as _ from 'lodash';
@@ -16,50 +16,50 @@ export let ApiDef = {
 @Injectable()
 export class ContentedService {
 
-    public options: RequestOptions;
-    constructor(private http: Http) {
-        let headers = new Headers({
+    public options = null;
+    constructor(private http: HttpClient) {
+        let headers = new HttpHeaders({
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         });
-        this.options = new RequestOptions({ headers: headers });
+        this.options = {headers: headers};
     }
 
     public getPreview() {
         return this.http.get(ApiDef.contented.preview, this.options)
-          .map(res => res.json())
           .catch(err => this.handleError(err));
     }
 
     public getFullDirectory(dir: string) {
         let url = ApiDef.contented.fulldir.replace('{dir}', dir);
         return this.http.get(url, this.options)
-          .map(res => res.json())
           .catch(err => this.handleError(err));
     }
 
-    public handleError(err: any) {
-        console.error("Failed to load this call", err);
-        let parsed = err;
-        if (err && err._body) {
-            try {
-                parsed = JSON.parse(err._body);
-                parsed['status'] = err.status;
+    public handleError(err: HttpErrorResponse) {
+        console.error("Failed to handle API call error", err);
+        let parsed = {};
+        if (_.isObject(err.error)) {
+            parsed = _.clone(err.error);
+        } else {
+           try {
+                parsed = JSON.parse(err.error) || {};
                 if (_.isEmpty(parsed)) {
-                    parsed = {
-                        error: "No actual response data or response was somehow empty.",
-                        debug: err._body,
-                        status: err.status
-                    };
+                    parsed = {error: "No response error, are you logged in?", debug: err.error};
                 }
             } catch (e) {
+                console.error("Failed to parse the json result from the API call.");
                 parsed = {
-                    "error": !_.isEmpty(err._body) ? "No JSON was returned in this error" : "Unknown error has occurred.",
-                    "status": err.status,
-                    "debug": err._body
+                    error: !_.isEmpty(err.error) ? "Exception, non json returned." : "Unhandled exception on the server.",
+                    debug: err.error
                 };
             }
         }
+        if (_.isEmpty(parsed)) {
+            parsed = {error: 'Unknown error, or no error text in the result?'};
+        }
+        parsed['url'] = err.url;
+        parsed['code'] = err.status;
         return Observable.fromPromise(Promise.reject(parsed));
     }
 }
