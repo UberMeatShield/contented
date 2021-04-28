8544dae346f6bd568f869e15b4ee351ab622e73e
UberMeatShield <justinc4@gmail.com>
2021-04-28 13:29:43 -0400

    Do not check in the index...

--- actions/app.go
+++ actions/app.go
@@ -2,17 +2,17 @@ package actions
 
 import (
     "log"
-	"contented/models"
-	"net/http"
-	"github.com/gobuffalo/buffalo"
-	"github.com/gobuffalo/buffalo-pop/v2/pop/popmw"
-	"github.com/gobuffalo/envy"
-	contenttype "github.com/gobuffalo/mw-contenttype"
-	forcessl "github.com/gobuffalo/mw-forcessl"
-	paramlogger "github.com/gobuffalo/mw-paramlogger"
-	"github.com/gobuffalo/x/sessions"
-	"github.com/rs/cors"
-	"github.com/unrolled/secure"
+    "contented/models"
+    "net/http"
+    "github.com/gobuffalo/buffalo"
+    "github.com/gobuffalo/buffalo-pop/v2/pop/popmw"
+    "github.com/gobuffalo/envy"
+    contenttype "github.com/gobuffalo/mw-contenttype"
+    forcessl "github.com/gobuffalo/mw-forcessl"
+    paramlogger "github.com/gobuffalo/mw-paramlogger"
+    "github.com/gobuffalo/x/sessions"
+    "github.com/rs/cors"
+    "github.com/unrolled/secure"
 )
 
 // ENV is used to help switch settings based on where the
@@ -36,54 +36,54 @@ var app *buffalo.App
 // placed last in the route declarations, as it will prevent routes
 // declared after it to never be called.
 func App(UseDatabase bool) *buffalo.App {
-	if app == nil {
-		app = buffalo.New(buffalo.Options{
-			Env:          ENV,
-			SessionStore: sessions.Null{},
-			PreWares: []buffalo.PreWare{
-				cors.Default().Handler,
-			},
-			SessionName: "_content_session",
-		})
+    if app == nil {
+        app = buffalo.New(buffalo.Options{
+            Env:          ENV,
+            SessionStore: sessions.Null{},
+            PreWares: []buffalo.PreWare{
+                cors.Default().Handler,
+            },
+            SessionName: "_content_session",
+        })
 
-		// Automatically redirect to SSL
-		app.Use(forceSSL())
+        // Automatically redirect to SSL
+        app.Use(forceSSL())
 
-		// Log request parameters (filters apply) this should maybe be only in dev
-		app.Use(paramlogger.ParameterLogger)
+        // Log request parameters (filters apply) this should maybe be only in dev
+        app.Use(paramlogger.ParameterLogger)
 
-		// Wraps each request in a transaction. Remove to disable this.
-		//  c.Value("tx").(*pop.Connection)
+        // Wraps each request in a transaction. Remove to disable this.
+        //  c.Value("tx").(*pop.Connection)
         if UseDatabase == true {
             log.Printf("Connecting to the database %b \n", UseDatabase)
-		    app.Use(popmw.Transaction(models.DB))
+            app.Use(popmw.Transaction(models.DB))
         } else {
             log.Printf("This code will attempt to use memory management %b \n", UseDatabase)
         }
 
-		// Set the request content type to JSON
-		app.Use(contenttype.Set("application/json"))
+        // Set the request content type to JSON
+        app.Use(contenttype.Set("application/json"))
 
-		// Run grift?  Do dev from an actual DB instance?
-		app.GET("/preview/{file_id}", PreviewHandler)
-		app.GET("/view/{file_id}", FullHandler)
-		app.GET("/download/{file_id}", DownloadHandler)
+        // Run grift?  Do dev from an actual DB instance?
+        app.GET("/preview/{file_id}", PreviewHandler)
+        app.GET("/view/{file_id}", FullHandler)
+        app.GET("/download/{file_id}", DownloadHandler)
 
-		// Host the index.html, also assume that all angular UI routes are going to be under contented
-		app.GET("/", AngularIndex)
-		app.GET("/ui/{path}", AngularIndex)
-		app.GET("/ui/{path}/{idx}", AngularIndex)
+        // Host the index.html, also assume that all angular UI routes are going to be under contented
+        app.GET("/", AngularIndex)
+        app.GET("/ui/{path}", AngularIndex)
+        app.GET("/ui/{path}/{idx}", AngularIndex)
 
-		// Need to make the file serving location smarter (serve the dir + serve static?)
-		app.ServeFiles("/public/build", http.Dir("public/build"))
-		app.ServeFiles("/public/css", http.Dir("public/css"))
+        // Need to make the file serving location smarter (serve the dir + serve static?)
+        app.ServeFiles("/public/build", http.Dir("public/build"))
+        app.ServeFiles("/public/css", http.Dir("public/css"))
 
-		// The DIR env environment is then served under /static (see actions.SetupContented)
+        // The DIR env environment is then served under /static (see actions.SetupContented)
         cr := app.Resource("/containers", ContainersResource{})
         cr.Resource("/media", MediaContainersResource{})
-		app.Resource("/media", MediaContainersResource{})
-	}
-	return app
+        app.Resource("/media", MediaContainersResource{})
+    }
+    return app
 }
 
 // forceSSL will return a middleware that will redirect an incoming request
@@ -92,8 +92,8 @@ func App(UseDatabase bool) *buffalo.App {
 // we recommend using a proxy: https://gobuffalo.io/en/docs/proxy
 // for more information: https://github.com/unrolled/secure/
 func forceSSL() buffalo.MiddlewareFunc {
-	return forcessl.Middleware(secure.Options{
-		SSLRedirect:     ENV == "production",
-		SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
-	})
+    return forcessl.Middleware(secure.Options{
+        SSLRedirect:     ENV == "production",
+        SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
+    })
 }
