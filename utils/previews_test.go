d257162e9234fa199e0dbb269467243e89ca5e56
Justin Carlson <jcarlson@secureworks.com>
2022-02-24 10:25:52 -0500

    how does golang keep getting tabs...

--- utils/previews_test.go
+++ utils/previews_test.go
@@ -1,73 +1,73 @@
 package utils
 
 import (
-	"os"
+    "os"
 //    "errors"
-	"path/filepath"
-	"testing"
+    "path/filepath"
+    "testing"
  //   "contented/models"
-	"github.com/gobuffalo/envy"
+    "github.com/gobuffalo/envy"
 )
 
 // Possibly make this some sort of global test helper function (harder to do in GoLang?)
 func Test_JpegPreview(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir1")
-	dstDir := GetPreviewDst(srcDir)
-	testFile := "this_is_jp_eg"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir1")
+    dstDir := GetPreviewDst(srcDir)
+    testFile := "this_is_jp_eg"
 
-	ResetPreviewDir(dstDir)
+    ResetPreviewDir(dstDir)
 
-	var size int64 = 20000
+    var size int64 = 20000
 
-	checkFile, _ := os.Open(filepath.Join(srcDir, testFile))
-	if ShouldCreatePreview(checkFile, size) == true {
-		st, _ := checkFile.Stat()
-		t.Errorf("Error, this should be too small file size was: %d", st.Size())
-	}
+    checkFile, _ := os.Open(filepath.Join(srcDir, testFile))
+    if ShouldCreatePreview(checkFile, size) == true {
+        st, _ := checkFile.Stat()
+        t.Errorf("Error, this should be too small file size was: %d", st.Size())
+    }
 
-	expectNoPreview, err := GetImagePreview(srcDir, testFile, dstDir, size)
-	if err != nil {
-		t.Errorf("Failed to get a preview %v", err)
-	}
-	if expectNoPreview != testFile && expectNoPreview != "" {
-		t.Errorf("File too small for psize found  %s and expected %s", expectNoPreview, testFile)
-	}
+    expectNoPreview, err := GetImagePreview(srcDir, testFile, dstDir, size)
+    if err != nil {
+        t.Errorf("Failed to get a preview %v", err)
+    }
+    if expectNoPreview != testFile && expectNoPreview != "" {
+        t.Errorf("File too small for psize found  %s and expected %s", expectNoPreview, testFile)
+    }
 
-	pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
-	if err != nil {
-		t.Errorf("Error occurred creating preview %v", err)
-	}
-	expectDst := filepath.Join(dstDir, testFile)
-	if expectDst != pLoc {
-		t.Errorf("Failed to find the expected file location %s had %s", expectDst, pLoc)
-	}
+    pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
+    if err != nil {
+        t.Errorf("Error occurred creating preview %v", err)
+    }
+    expectDst := filepath.Join(dstDir, testFile)
+    if expectDst != pLoc {
+        t.Errorf("Failed to find the expected file location %s had %s", expectDst, pLoc)
+    }
 }
 
 // Does it work when there is a png
 func Test_PngPreview(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir1")
-	dstDir := GetPreviewDst(srcDir)
-	testFile := "this_is_p_ng"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir1")
+    dstDir := GetPreviewDst(srcDir)
+    testFile := "this_is_p_ng"
 
-	// Add a before each to nuke the dstDir and create it
-	ResetPreviewDir(dstDir)
-	pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
-	if err != nil {
-		t.Errorf("Failed to get a preview %v", err)
-	}
-	expectDst := filepath.Join(dstDir, testFile)
-	if expectDst != pLoc {
-		t.Errorf("Failed to find the expected location %s was %s", expectDst, pLoc)
-	}
+    // Add a before each to nuke the dstDir and create it
+    ResetPreviewDir(dstDir)
+    pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
+    if err != nil {
+        t.Errorf("Failed to get a preview %v", err)
+    }
+    expectDst := filepath.Join(dstDir, testFile)
+    if expectDst != pLoc {
+        t.Errorf("Failed to find the expected location %s was %s", expectDst, pLoc)
+    }
 }
 
 // We know this file is 10.08 seconds long
 func Test_VideoLength(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir2")
-	testFile := "donut.mp4"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir2")
+    testFile := "donut.mp4"
 
     srcFile := filepath.Join(srcDir, testFile)
     checkLen, fps, err := GetTotalVideoLength(srcFile)
@@ -84,11 +84,11 @@ func Test_VideoLength(t *testing.T) {
 
 // Test MultiScreen
 func Test_MultiScreen(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir2")
-	testFile := "donut.mp4"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir2")
+    testFile := "donut.mp4"
     dstDir := GetPreviewDst(srcDir)
-	ResetPreviewDir(dstDir)
+    ResetPreviewDir(dstDir)
 
     destFile := filepath.Join(dstDir, "donut.png")
     srcFile := filepath.Join(srcDir, testFile)
@@ -105,14 +105,14 @@ func Test_MultiScreen(t *testing.T) {
 
 
 func Test_BrokenImagePreview(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir3")
-	dstDir := GetPreviewDst(srcDir)
-	testFile := "nature-corrupted-free-use.jpg"
-	ResetPreviewDir(dstDir)
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir3")
+    dstDir := GetPreviewDst(srcDir)
+    testFile := "nature-corrupted-free-use.jpg"
+    ResetPreviewDir(dstDir)
 
     // TODO: This needs to be made into a better place around previews
-	pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 0)
+    pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 0)
     if err == nil {
         t.Errorf("This file should definitely cause an error")
     }
@@ -123,25 +123,25 @@ func Test_BrokenImagePreview(t *testing.T) {
 
 // Makes it so that the preview is generated
 func Test_VideoPreviewPNG(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir2")
-	dstDir := GetPreviewDst(srcDir)
-	testFile := "donut.mp4"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir2")
+    dstDir := GetPreviewDst(srcDir)
+    testFile := "donut.mp4"
 
-	// Add a before each to nuke the dstDir and create it
-	ResetPreviewDir(dstDir)
+    // Add a before each to nuke the dstDir and create it
+    ResetPreviewDir(dstDir)
     expectDst, dErr := ErrorOnPreviewExists(testFile, dstDir, "video/hack")
     if dErr != nil {
         t.Errorf("The dest file already exists %s\n", expectDst)
     }
 
-	pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
-	if err != nil {
-		t.Errorf("Failed to get Video preview %v", err)
-	}
-	if expectDst != pLoc {
-		t.Errorf("Failed to find the expected location %s was %s", expectDst, pLoc)
-	}
+    pLoc, err := GetImagePreview(srcDir, testFile, dstDir, 10)
+    if err != nil {
+        t.Errorf("Failed to get Video preview %v", err)
+    }
+    if expectDst != pLoc {
+        t.Errorf("Failed to find the expected location %s was %s", expectDst, pLoc)
+    }
     // TODO: Figure out sime sizing constraints
     _, no_file_err := os.Stat(pLoc); 
     if no_file_err != nil {
@@ -152,12 +152,12 @@ func Test_VideoPreviewPNG(t *testing.T) {
 
 
 func Test_FileExistsError(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir1")
-	dstDir := GetPreviewDst(srcDir)
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir1")
+    dstDir := GetPreviewDst(srcDir)
     knownFile := "0_LargeScreen.png"
 
-	ResetPreviewDir(dstDir)
+    ResetPreviewDir(dstDir)
     fqPath := GetPreviewPathDestination(knownFile, dstDir, "image/png")
     f, err := os.Create(fqPath)
     if err != nil {
@@ -180,12 +180,12 @@ func Test_FileExistsError(t *testing.T) {
 }
 
 func Test_VideoPreviewGif(t *testing.T) {
-	var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir2")
-	dstDir := GetPreviewDst(srcDir)
-	testFile := "donut.mp4"
+    var testDir, _ = envy.MustGet("DIR")
+    srcDir := filepath.Join(testDir, "dir2")
+    dstDir := GetPreviewDst(srcDir)
+    testFile := "donut.mp4"
 
-	ResetPreviewDir(dstDir)
+    ResetPreviewDir(dstDir)
     expectDst, dErr := ErrorOnPreviewExists(testFile, dstDir, "video/hack")
     if dErr != nil {
         t.Errorf("The dest file already exists %s\n", expectDst)
@@ -207,22 +207,22 @@ func Test_VideoPreviewGif(t *testing.T) {
 
 func Test_ShouldCreate(t *testing.T) {
     var testDir, _ = envy.MustGet("DIR")
-	srcDir := filepath.Join(testDir, "dir1")
-	testFile := "this_is_p_ng"
+    srcDir := filepath.Join(testDir, "dir1")
+    testFile := "this_is_p_ng"
 
-	filename := filepath.Join(srcDir, testFile)
-	srcImg, fErr := os.Open(filename)
+    filename := filepath.Join(srcDir, testFile)
+    srcImg, fErr := os.Open(filename)
 
-	if fErr != nil {
-		t.Errorf("This file cannot be opened %s with err %s", filename, fErr)
-	}
+    if fErr != nil {
+        t.Errorf("This file cannot be opened %s with err %s", filename, fErr)
+    }
 
-	preview_no := ShouldCreatePreview(srcImg, 30000)
-	if preview_no != false {
-		t.Errorf("This preview should not be created")
-	}
-	preview_yes := ShouldCreatePreview(srcImg, 1000)
-	if preview_yes != true {
-		t.Errorf("At this size it should create a preview")
-	}
+    preview_no := ShouldCreatePreview(srcImg, 30000)
+    if preview_no != false {
+        t.Errorf("This preview should not be created")
+    }
+    preview_yes := ShouldCreatePreview(srcImg, 1000)
+    if preview_yes != true {
+        t.Errorf("At this size it should create a preview")
+    }
 }
