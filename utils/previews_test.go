3b3275a7b585ae40e2bffec51c2c5b4f661c6a0c
Justin Carlson <jcarlson@secureworks.com>
2022-04-15 15:30:38 -0400

    Working tests.

--- utils/previews_test.go
+++ utils/previews_test.go
@@ -109,7 +109,7 @@ func Test_MultiScreen(t *testing.T) {
         t.Errorf("Did not get a valid destination file.")
     }
     screens_check, _ := ioutil.ReadDir(dstDir)
-    expected := 11
+    expected := 10
     if len(screens_check) != expected {
         t.Errorf("Not enough screens created %d vs expected %d", len(screens_check), expected)
     }
