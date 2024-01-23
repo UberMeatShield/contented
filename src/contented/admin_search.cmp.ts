46e6c648a98b694f88d690e37cc6a8b769f8027f
Justin <justinc4@gmail.com>
2024-01-23 15:04:55 -0500

    Hover color.

--- src/contented/admin_search.cmp.ts
+++ src/contented/admin_search.cmp.ts
@@ -5,6 +5,8 @@ import {
 import {Content} from './content';
 
 
+// TODO: When styling out the search add a hover and hover text to make it 
+// more obvious when something can be clicked.
 @Component({
     selector: 'admin-search-cmp',
     templateUrl: './search.ng.html'
